cc7b193c0ab0a371fbf680f76f76d24d525ea35f
bowopusdik <bowopusdik@gmail.com>
2026-07-03 10:32:18 +0700

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./pages/Dashboard";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-spj-bg text-gray-800">
-      <Dashboard />
-    </div>
-  );
-}
