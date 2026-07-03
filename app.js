8c3a1187723a335529c2a6a291194fffd9c724ba
bowopusdik <bowopusdik@gmail.com>
2026-07-03 10:44:22 +0700

    Add modal open and close functionality

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// BUKA MODAL
+function openModal() {
+  document.getElementById("spjModal").style.display = "flex";
+}
 
+// TUTUP MODAL
+function closeModal() {
+  document.getElementById("spjModal").style.display = "none";
+}
+
+// KLIK DI LUAR MODAL UNTUK TUTUP
+window.onclick = function(event) {
+  let modal = document.getElementById("spjModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
